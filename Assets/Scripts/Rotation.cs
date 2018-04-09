aa64382c70d966ba3804fec91a519a6db3b68fd8
BUGMINI-PC\Айнур <aynuralmurzina@gmail.com>
2018-04-09 11:18:52 +0600

    Base project

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+
+	public Transform rotateAround;
+	public float speed = -20f;
+
+	void Update () {
+		transform.RotateAround(rotateAround.position, Vector3.forward, speed * Time.deltaTime);
+	}
+}
